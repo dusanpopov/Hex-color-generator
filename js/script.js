e455deea1cfaae24a655cfc6bf7edf4c088253d4
dusanpopov <aristocut90@gmail.com>
2018-12-17 22:57:51 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const hexCode = document.querySelector(".hex--code");
+const colorPreview = document.querySelector(".color--preview");
+const btn = document.querySelector(".btn");
+
+
+
+const createHexValue = () =>{
+
+    const hexValues = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++){
+
+        let randomValue = Math.floor(Math.random() * hexValues.length);
+
+        hexColor += hexValues[randomValue];
+    }
+
+    colorPreview.style.backgroundColor = hexColor;
+    
+    hexCode.textContent = hexColor;
+}
+
+
+
+btn.addEventListener("click", createHexValue);
