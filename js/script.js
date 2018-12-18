936ec18971ccc791238084d0e459b7ffcac3f78b
Dušan Popov <aristocut90@gmail.com>
2018-12-18 19:17:08 +0100

    Update script.js
    added toLowerCase() method to meet CSS - General Formatting Rules - Capitalization standard

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ const colorPreview = document.querySelector(".color--preview");
 const btn = document.querySelector(".btn");
 
 
-
-const createHexValue = () =>{
+const createHexValue = () => {
 
     const hexValues = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 
     let hexColor = "#";
 
-    for (let i = 0; i < 6; i++){
+    for (let i = 0; i < 6; i++) {
 
         let randomValue = Math.floor(Math.random() * hexValues.length);
 
@@ -19,7 +18,7 @@ const createHexValue = () =>{
 
     colorPreview.style.backgroundColor = hexColor;
     
-    hexCode.textContent = hexColor;
+    hexCode.textContent = hexColor.toLowerCase();
 }
 
 
