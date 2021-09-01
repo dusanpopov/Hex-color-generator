c0159688c1a55144b1b374eb2b48d8d56edbbf3d
Dušan Popov <aristocut90@gmail.com>
2021-09-01 05:26:22 +0200

    formatting, added arrow function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
-const hexCode = document.querySelector(".hex--code");
-const colorPreview = document.querySelector(".color--preview");
+const hexCode = document.querySelector(".hex__code");
+const colorPreview = document.querySelector(".color__preview");
 const btn = document.querySelector(".btn");
 
-
-function createHexValue(){
-    
+const createHexValue = () => {
     const hexValues = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-
     let hexColor = "#";
 
     for (let i = 0; i < 6; i++) {
-
         let randomValue = Math.floor(Math.random() * hexValues.length);
-
         hexColor += hexValues[randomValue];
     }
 
     colorPreview.style.backgroundColor = hexColor;
-    
     hexCode.textContent = hexColor.toLowerCase();
 }
 
