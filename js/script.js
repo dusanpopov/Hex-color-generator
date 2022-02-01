8a643e11a7074d874d2dc42bb029d7b6a5b3ac2a
Dušan Popov <aristocut90@gmail.com>
2022-02-01 01:58:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,14 +5,12 @@ const btn = document.querySelector(".btn");
 const createHexValue = () => {
     const hexValues = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
     let hexColor = "#";
-
+    
     for (let i = 0; i < 6; i++) {
         let randomValue = Math.floor(Math.random() * hexValues.length);
         hexColor += hexValues[randomValue];
     }
-
     colorPreview.style.backgroundColor = hexColor;
     hexCode.textContent = hexColor.toLowerCase();
 }
-
 btn.addEventListener("click", createHexValue);
